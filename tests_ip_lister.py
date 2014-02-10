530b3de2b23ebe6fe8902d3d18389f36ea121c1d
Jesse Evers <jesse.e.evers@lmco.com>
2014-02-10 18:56:03 -0500

    pylint changes

--- tests_ip_lister.py
+++ tests_ip_lister.py
@@ -1,5 +1,11 @@
+'''
+Tests for storage_ip_lister
+'''
+
 from sh import date
 
 def test_sh_date():
-        date_out = date()
-        assert date_out.exit_code == 0
+    '''Some lame test that nose can pick up.'''
+    date_out = date()
+    assert date_out.exit_code == 0
+
