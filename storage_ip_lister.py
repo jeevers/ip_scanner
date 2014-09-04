add379e59e7811a7816a5ba670ed2fae7bc4a244
Jesse Evers <jesse.e.evers@lmco.com>
2014-09-04 16:32:23 -0400

    config now pulled from YAML file; performs reverse lookup on both active and inactive IPs

--- storage_ip_lister.py
+++ storage_ip_lister.py
@@ -1,62 +1,54 @@
 #!/usr/bin/env python
-from sh import nmap
 import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
 import os
 import re
+import yaml
 
-ip_nets = ["192.168.11.",
-       "192.168.12.",
-       "192.168.15.",
-       "192.168.22.",
-       "192.168.122.",
-       "192.168.115."]
-#ip_nets = ["192.168.11."]
-port_listen = 8068
-poll_interval = 20
-ip_scan = []
+from sh import nmap
+from dns import reversename, resolver
+
+
+def config():
+    config_path = os.path.join(os.path.dirname(__file__), 'config.yaml')
+    with file(config_path, 'r') as config_file:
+        config_dict = yaml.load(config_file)
+        return config_dict
+
 
 def disp_storage_ips(subnets):
-    nmap_args = ["-sP"] + [item+"0/24" for item in subnets]
+    nmap_args = ["-nsP"] + [item+"0/24" for item in subnets]
     vlans = nmap(nmap_args)
     ip_patt = '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
     p = re.compile(ip_patt)
-    p.findall(vlans.stdout)
-
-    dns_resolved = "Host (.*) \(([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\) appears to be up"
-    resolved_re = re.compile(dns_resolved)
-    resolved = resolved_re.findall(vlans.stdout)
-
-    hostname_dict = {}
-
-    for record in resolved:
-        hostname_dict[record[1]] = record[0]
+    #p.findall(vlans.stdout)
+    active_ips = p.findall(vlans.stdout)
 
     storage_ips = [[subnet+str(last_oct) for last_oct in range(1,256)] for subnet in subnets]
-
-    active_ips = p.findall(vlans.stdout)
 
     ip_usage = []
 
     for vlan in storage_ips:
         vlan_list = []
         for ip in vlan:
+            try:
+                host = reversename.from_address(ip)
+                name = resolver.query(host, 'PTR')[0].to_text()
+            except resolver.NXDOMAIN:
+                name = ''
             if ip in active_ips:
-                try:
-                    vlan_list.append([ip, 'ACTIVE', hostname_dict[ip]]) 
-                except KeyError:
-                    vlan_list.append([ip, 'ACTIVE', ''])
+                vlan_list.append([ip, 'ACTIVE', name])
             else:
-                vlan_list.append([ip, 'INACTIVE', ''])
+                vlan_list.append([ip, 'INACTIVE', name])
         ip_usage.append(vlan_list)
     return ip_usage
 
 def ip_poll():
     print("ip scan start")
     global ip_scan
-    ip_scan = disp_storage_ips(ip_nets)
+    ip_scan = disp_storage_ips(config()['subnets'])
     print("ip scan stop")
 
 class mainHandler(tornado.web.RequestHandler):
@@ -80,7 +72,7 @@ application = tornado.web.Application([
 if __name__ == '__main__':
     import signal
     http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(port_listen)
+    http_server.listen(config()['port_listen'])
     loop = tornado.ioloop.IOLoop.instance()
     def sigint_handler(signum, frame):
         print('signal handler called with %s, frame %s' % (signum, frame))
@@ -89,6 +81,7 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, sigint_handler)
     signal.signal(signal.SIGHUP, sigint_handler)
     signal.signal(signal.SIGTERM, sigint_handler)
+    poll_interval = config()['poll_interval']
     periodic_cbk = tornado.ioloop.PeriodicCallback(ip_poll,
                                                    poll_interval*60*1000,
                                                    loop)
